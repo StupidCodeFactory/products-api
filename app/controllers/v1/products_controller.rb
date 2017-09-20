cd25fad719cacc727d203feb394287c02564c142
yann marquet <ymarquet@gmail.com>
2017-09-20 19:39:11 +0100

    refactor products controller

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -12,25 +12,13 @@ class V1::ProductsController < ApplicationController
 
   def create
     product = Product.new(product_params)
-
-    if product.valid?
-      product.save
-      render json: product
-    else
-      render json: { error: product.errors }, status: :unprocessable_entity
-    end
+    save_and_render(product)
   end
 
   def update
     product = Product.find(params.require(:id))
     product.assign_attributes(product_params)
-
-    if product.valid?
-      product.save
-      render json: product
-    else
-      render json: { error: product.errors }, status: :unprocessable_entity
-    end
+    save_and_render(product)
   end
 
   def destroy
@@ -38,11 +26,21 @@ class V1::ProductsController < ApplicationController
     product.destroy!
     head :ok
   end
+
   private
 
   def product_params
     params.require(:product).permit(
       :name, :description, :price_in_cents
     )
+  end
+
+  def save_and_render(product)
+    if product.save
+      render json: product
+    else
+      render json: { error: product.errors }, status: :unprocessable_entity
+    end
+
   end
 end
