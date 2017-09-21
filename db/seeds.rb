07119241985bcb990a7a3adf5e08b35787210680
yann marquet <ymarquet@gmail.com>
2017-09-21 01:03:12 +0100

    dockerise application

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-FactoryGirl.create_list :product, 50
+FactoryGirl.create_list :product, 5
